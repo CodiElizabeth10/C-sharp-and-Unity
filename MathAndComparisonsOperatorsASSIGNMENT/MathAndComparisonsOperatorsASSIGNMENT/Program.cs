a68b980ca13db9678408e9b8cf9654377e470a37
Codi <codifalterman@gmail.com>
2022-04-21 14:39:55 -0500

    DailyReport

---
+++ MathAndComparisonsOperatorsASSIGNMENT/MathAndComparisonsOperatorsASSIGNMENT/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonsOperatorsASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+            int product = 15 * 2080 ;
+            Console.WriteLine("Annual salary of Person 1: " + product);
+           
+
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 2");
+            int product1 = 20 * 2080;
+            Console.WriteLine("Annual salary of Person 2: " + product1);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
+
