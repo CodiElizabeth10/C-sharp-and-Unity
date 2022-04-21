843e4baa046b41b1523e7b454fb2b6ce3d05f6c6
Codi <codifalterman@gmail.com>
2022-04-21 15:16:50 -0500

    Math Comparisons Assignment

--- MathComparisons3/MathComparisons3/Program.cs
+++ MathComparisons3/MathComparisons3/Program.cs
@@ -12,18 +12,41 @@ namespace MathAndComparisonsOperatorsASSIGNMENT
         {
             Console.WriteLine("Anonymous Income Comparison Program");
             Console.WriteLine("Person 1");
-            int product = 15 * 2080;
-            Console.WriteLine("Annual salary of Person 1: " + product);
+            Console.WriteLine("Hourly Rate?");
+            int hourlyRate = 15;
+            Console.WriteLine(hourlyRate);
+            Console.WriteLine("Hours Worked Per Week?");
+            int hoursWeek = 40;
+            Console.WriteLine(hoursWeek);
 
 
-            Console.WriteLine("Anonymous Income Comparison Program");
+
             Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Rate?");
+            int hourlyWage = 20;
+            Console.WriteLine(hourlyWage);
+            Console.WriteLine("Hours Worked Per Week?");
+            int weeklyHours = 40;
+            Console.WriteLine(weeklyHours);
+
+
+
+            Console.WriteLine("Annual salary of Person 1:");
+            int product = 15 * 2080;
+            Console.WriteLine(product);
+
+            Console.WriteLine("Annual salary of Person 2");
             int product1 = 20 * 2080;
-            Console.WriteLine("Annual salary of Person 2: " + product1);
+            Console.WriteLine(product1);
+            
+
+            int personOne = 31200;
+            int personTwo = 41600;
+
+            bool makesMore = personOne > personTwo;
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            Console.WriteLine(makesMore);
             Console.ReadLine();
-
-
-
 
         }
     }
