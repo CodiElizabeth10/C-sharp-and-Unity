fe76f1f4829e53e61ffc9e69e22f2e1061ce6b5a
Codi <codifalterman@gmail.com>
2022-04-21 14:51:31 -0500

    Math and Comparisons

--- MathComparisons3/MathComparisons3/Properties/AssemblyInfo.cs
+++ MathComparisons3/MathComparisons3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MathAndComparisonsOperatorsASSIGNMENT")]
+[assembly: AssemblyTitle("MathComparisons3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MathAndComparisonsOperatorsASSIGNMENT")]
+[assembly: AssemblyProduct("MathComparisons3")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("18c1e3c5-4d52-4e38-ac12-3ad603dc9754")]
+[assembly: Guid("0bf722cf-e3f9-4b0d-a7e5-48f5a37350d0")]
 
 // Version information for an assembly consists of the following four values:
 //
