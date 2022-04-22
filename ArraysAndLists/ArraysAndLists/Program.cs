b5416aff876e4773405ee7a466da4236dab61569
Codi <codifalterman@gmail.com>
2022-04-22 15:14:58 -0500

    Arrays and Lists

---
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        //EXAMPLES OF WAYS YOU CAN WRITE ARRAYS 
+        static void Main()
+        {
+            List<string> intList = new List<string>();
+            intList.Add("Hello");
+            intList.Add("Codi");
+            intList.Remove("Codi");
+            //List<int> intList = new List<int>();
+            //intList.Add(4);
+            //intList.Add(10);
+            //intList.Remove(10);
+
+            Console.WriteLine(intList[0]);
+            Console.ReadLine();
+
+            Console.WriteLine(intList[0]);
+            Console.ReadLine();
+
+            byte[5000] byteArray = new byte[5000];
+
+            //int[] numArray = new int[5];
+            //numArray[0] = 5;
+            //numArray[1] = 2;
+            //numArray[2] = 10;
+            //numArray[3] = 200;
+            //numArray[4] = 5000;
+
+            //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000, 300, 90, 95, 2300 };
+
+            //int[] numArray2 = { 5, 2, 10, 200, 500, 50000 };
+
+            //numArray2[5] = 650;
+
+
+            //Console.WriteLine(numArray2[5]);
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ ArraysAssignment/ArraysAssignment/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite summer month?");
+            string[] summerMonths = { "June", "July", "August" };
+            Console.ReadLine();
+
+            Console.WriteLine(summerMonths[0]);
+            string choseJun = Console.ReadLine();
+            Console.WriteLine("You chose, " + choseJun);
+            Console.ReadLine();
+
+            Console.WriteLine(summerMonths[1]);
+            string choseJul = Console.ReadLine();
+            Console.WriteLine("You chose, " + choseJul);
+            Console.ReadLine();
+
+            Console.WriteLine(summerMonths[2]);
+            string choseAug = Console.ReadLine();
+            Console.WriteLine("You chose, " + choseAug);
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+            //ARRAY OF INTEGERS
+            //{
+            //        Console.WriteLine("Select a number:");
+            //        int number = Convert.ToInt32(Console.ReadLine());
+            //        int[] numArray = new int[] { 100, 200, 110, 2000, };
+
+
+//        Console.WriteLine(numArray[0]);
+//        Console.WriteLine("You chose " + numArray[0]);
+//        Console.WriteLine(numArray[1]);
+//        Console.WriteLine("You chose " + numArray[1]);
+//        Console.WriteLine(numArray[2]);
+//        Console.WriteLine("You chose " + numArray[2]);
+//        Console.WriteLine(numArray[3]);
+//        Console.WriteLine("You chose " + numArray[3]);
+//        Console.ReadLine();
+//    }
+//}
+//}
+
+
+
+//ONE DIMENSIONAL ARRAY OF STRINGS
+//Console.WriteLine("Select an animal:");
+//Console.ReadLine();
+//List<string> intList = new List<string>();
+//{ 
+//    intList.Add("Bear");
+//    intList.Add("Fox");
+//    intList.Add("Dolphin");
+//    intList.Add("Wolf");
+//    Console.ReadLine();
+//    Console.WriteLine("You chose " + intList[0]);
+//    Console.ReadLine();
+//    Console.WriteLine("You chose " + intList[1]);
+//    Console.ReadLine();
+//    Console.WriteLine("You chose " + intList[2]);
+//    Console.ReadLine();
+//    Console.WriteLine("You chose " + intList[3]);
+//    Console.ReadLine();
+
+
