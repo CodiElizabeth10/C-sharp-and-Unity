6115cce19a3ec4a8fa7ee92b920701ebafab9572
Codi <codifalterman@gmail.com>
2022-04-22 18:44:29 -0500

    6 part assgnmnt

---
+++ 6PartCSharpAssignment/6PartCSharpAssignment/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6PartCSharpAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ASSIGNMENT ONE
+
+            //string[] states = { "Montana", "Louisiana", "indiana"};
+
+            // for (int j = 0; j < states.Length; j++)
+            // {
+            //     Console.WriteLine(states[j]);
+            // }
+            // Console.WriteLine("Please enter an index from 0 to 2:");
+            // int userIndex = Convert.ToInt32(Console.ReadLine());
+            // if (userIndex >= 0 && userIndex <= 2)
+            // {
+            //     Console.WriteLine(states[userIndex]);
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Sorry, invalid index has been entered.");
+            // }
+            // Console.ReadLine();
+
+
+
+
+            //ASSIGNMENT TWO
+
+            //while (true)
+            //{
+            //    Console.WriteLine("How Do Trees Get Online? They Just Log In");
+            //}
+
+
+
+
+            //ASSIGNMENT THREE
+
+            //List<int> gameLevels = new List<int>();
+            //gameLevels.Add(98);
+            //gameLevels.Add(99);
+            //gameLevels.Add(81);
+            //gameLevels.Add(72);
+            //gameLevels.Add(70);
+
+            //foreach (int level in gameLevels)
+            //{
+            //    if (level > 80)
+            //    {
+            //        Console.WriteLine("Your level: " + level + " you may enter.");
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //List<int> gameLevels = new List<int>();
+            //gameLevels.Add(98);
+            //gameLevels.Add(99);
+            //gameLevels.Add(81);
+            //gameLevels.Add(72);
+            //gameLevels.Add(70);
+
+            //foreach (int level in gameLevels)
+            //{
+            //    if (level <= 79)
+            //    {
+            //        Console.WriteLine("Your level: " + level + " -- you do not meet the requirments to enter. Try again at level 80.");
+            //    }
+            //}
+            //Console.ReadLine();
+
+
+
+            //ASSIGNMENT FOUR
+
+            //List<string> intList = new List<string>();
+            //intList.Add("Beach");
+            //intList.Add("Mountains");
+            //intList.Add("Jungle");
+            //intList.Add("Antarctica");
+            //Console.WriteLine("Search for a destination by entering an index from 0 to 3:");
+            //int userIndex = Convert.ToInt32(Console.ReadLine());
+            //if (userIndex >= 0 && userIndex <= 3)
+            //{
+            //    Console.WriteLine(intList[userIndex]);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sorry, invalid index has been entered.");
+            //}
+            //Console.ReadLine();
+
+
+
+            //ASSIGNMENT FIVE
+
+            //List<string> names = new List<string>() { "Mia", "Mia", "Hailey", "Jared" };
+            //Console.WriteLine("Select and index 0-3 to search for a name");
+            //int userIndex = Convert.ToInt32(Console.ReadLine());
+
+            //    foreach (string name in names)
+            //   {
+            //    if (userIndex >= 0 && userIndex <= 3)
+            //    {
+            //        Console.WriteLine(names[userIndex]);
+            //    }
+            //    else
+
+            //    {
+            //        Console.WriteLine("Sorry, invalid index has been entered.");
+            //    }
+            //    Console.ReadLine();
+
+            //ASIIGNMENT SIX
+
+//            List<string> intList = new List<string>();
+//            intList.Add("Summer");
+//            intList.Add("Spring");
+//            intList.Add("Spring");
+//            intList.Add("Fall");
+//            Console.WriteLine("Please enter an index from 0 to 3:");
+//            int userIndex = Convert.ToInt32(Console.ReadLine());
+//            if (userIndex <= 0 || userIndex <= 1 || userIndex >= 3)
+//            {
+//                Console.WriteLine(intList[userIndex]);
+//            }
+//            else
+//            {
+//                Console.WriteLine("This has appeared on the list already.");
+//            }
+//            Console.ReadLine();
+
+
+//        }
+//    }
+//}
+
+
+
+
+
+
+
