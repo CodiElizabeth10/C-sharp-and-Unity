b241027d33cbfa962324e0acf523b453729a5066
Codi <codifalterman@gmail.com>
2022-04-23 18:21:27 -0500

    Polymorphism

---
+++ Polymorphism/Polymorphism/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Employee : Person, IQuittable
+    {
+
+        public override void DisplayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("I'm quitting (: ");
+        }
+    }
+}
