b2b0a01fa1444770f6498c810a1b9baa2cbb5791
Codi <codifalterman@gmail.com>
2022-04-23 15:01:18 -0500

    Main Method 2

---
+++ Main Method 2/Main Method 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please type integer one:");
+            int Ten = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please type second integer and press enter:");
+            string input = Console.ReadLine();
+            if (input != "")
+            {
+                int Eleven = Convert.ToInt32(input);
+                TwoNumbers.TwoNumbersChoose(Ten, Eleven);
+            }
+            else
+            {
+                TwoNumbers.TwoNumbersChoose(Ten);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Main Method 2/Main Method 2/TwoNumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_2
+{
+    class TwoNumbers
+    {
+        public static void TwoNumbersChoose(int Ten, int Eleven = 110)
+        {
+
+            Console.WriteLine(Ten * Eleven);
+        }
+    }
+}
