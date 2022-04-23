a601062a2ec202f78e281955abfa910f3268b90f
Codi <codifalterman@gmail.com>
2022-04-23 15:16:33 -0500

    Main Method 3

---
+++ Main Method 3/Main Method 3/Class0.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_3
+{
+    class Class0
+    {
+        public static void ImBadAtMath(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum * secondNum);
+        }
+    }
+}
---
+++ Main Method 3/Main Method 3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class0.ImBadAtMath(7, 9);
+            Class0.ImBadAtMath(firstNum: 5646, secondNum: 254);
+            Console.ReadLine();
+        }
+    }
+}
