2117ec5ce0a0986b9221c0e8ce873bb667126fb0
Codi <codifalterman@gmail.com>
2022-04-23 16:42:09 -0500

    Objects And Methods

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,20 +10,19 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+
             Deck deck = new Deck();
             deck.Shuffle(3);
-          
-
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
-            
+
             Console.ReadLine();
         }
 
-        
+
 
         //public static Deck Shuffle(Deck deck, int times)
         //{
