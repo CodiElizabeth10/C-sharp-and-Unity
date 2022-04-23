89f09efb8218fd4ae95a45bfc2e1e20441cb5470
Codi <codifalterman@gmail.com>
2022-04-23 14:49:29 -0500

    Main Methods Assignment

---
+++ Main Method/Main Method/Mathh.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Mathh
+    {
+
+        public static int lMath(int integer)
+        {
+            return integer + integer;
+        }
+        public static int lMath(decimal deceger)
+        {
+            return Convert.ToInt32(200 / deceger);
+        }
+        public static int lMath(string strinteger)
+        {
+            return 80 * Convert.ToInt32(strinteger);
+        }
+    }
+}
+
---
+++ Main Method/Main Method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Mathh.lMath(80));
+            Console.WriteLine(Mathh.lMath(.9m));
+            Console.WriteLine(Mathh.lMath("20"));
+            Console.ReadLine();
+        }
+    }
+}
