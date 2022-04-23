04d99bfd9298b479caa8a0f9f4ca217eced0daa3
Codi <codifalterman@gmail.com>
2022-04-22 20:10:16 -0500

    Exception Assignment

---
+++ ExceptionAssignment/ExceptionAssignment/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> intList = new List<int>();
+                intList.Add(400);
+                intList.Add(10);
+                intList.Add(12);
+                Console.WriteLine("Pick a number to divide by 400");
+                intList[0] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a number to divide by 10");
+                intList[1] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a number to divide by 12");
+                intList[3] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number.");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two...");
+                int numberThree = intList[0] / numberTwo;
+                Console.WriteLine(intList[0] + " divided by " + numberTwo + " = " + numberThree);
+                int numberFour = intList[1] / numberTwo;
+                Console.WriteLine(intList[1] + " divided by " + numberTwo + " = " + numberFour);
+                int numberFive = intList[2] / numberTwo;
+                Console.WriteLine(intList[2] + " divided by " + numberTwo + " = " + numberFive);
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>();
+            intList.Add(4);
+            intList.Add(10);
+            intList.Remove(10);
+
+            Console.WriteLine(intList[0]);
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
+
+//            try
+//            {
+
+//                Console.WriteLine("Pick a number from 100-200");
+//                int numberOne = Convert.ToInt32(Console.ReadLine());
+//                Console.WriteLine("Pick a second number.");
+//                int numberTwo = Convert.ToInt32(Console.ReadLine());
+//                Console.WriteLine("Dividing the two...");
+//                int numberThree = numberOne / numberTwo;
+//                Console.WriteLine(numberOne + " divided by " + numberTwo + " = " + numberThree);
+//                Console.ReadLine();
+//            }
+//            catch (FormatException ex)
+//            {
+//            }
+//            catch (DivideByZeroException ex)
+//            {
+//                Console.WriteLine("Please don't divide by zero");
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine(ex.Message);
+//            }
+//            finally
+//            {
+//                Console.ReadLine();
+
+//            }
+//            Console.ReadLine();
+
+//        }
+//    }
+//}
