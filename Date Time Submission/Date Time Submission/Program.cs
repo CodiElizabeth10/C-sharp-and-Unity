2755c2977892ff436a432115c7fd9c54ede12d42
Codi <codifalterman@gmail.com>
2022-04-24 14:14:41 -0500

    Date Time Submission

---
+++ Date Time Submission/Date Time Submission/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Time_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine(DateTime.Now);
+            Console.WriteLine("How long from now would you like to set an alarm for? (enter number for hours)");
+            int timespan = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(DateTime.Now.AddHours(timespan));
+
+            Console.Read();
+        }
+}
+}
